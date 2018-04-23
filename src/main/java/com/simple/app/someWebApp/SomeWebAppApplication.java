e29e6ac8264683e821aed58b248e136510a18afd
japshvincent <japshvincent@gmail.com>
2018-04-23 13:47:40 -0500

    init repo

---
+++ src/main/java/com/simple/app/someWebApp/SomeWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.simple.app.someWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeWebAppApplication.class, args);
+	}
+}
