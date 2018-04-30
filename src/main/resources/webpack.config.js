51d8a0185c957a8b6dddadd7fb757899e1d0ba0f
japshvincent <japshvincent@gmail.com>
2018-04-30 09:20:47 -0500

    adding bootstrap and jquery
    adding jquery and bootstrap using npm dependency.
    building jquery via webpack and css via grunt

--- src/main/resources/webpack.config.js
+++ src/main/resources/webpack.config.js
@@ -9,43 +9,11 @@ module.exports = {
         module1: './src/js/module1.js',
         module2: './src/js/module2.js',
         module3: './src/js/module3.js',
-        appBaseJs: './src/js/someSimpleAppBase.js',
-        appBaseCss: './src/scss/someSimpleAppBase.scss'
+        bootstrap: './src/js/bootstrap.js',
+        jquery: './src/js/jquery.js'
     },
     output: {
         path: PATHS.build,
         filename: '[name].min.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(scss)$/,
-                use: [
-                    {
-                        // Adds CSS to the DOM by injecting a `<style>` tag
-                        loader: 'style-loader'
-                    },
-                    {
-                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
-                        loader: 'css-loader'
-                    },
-                    {
-                        // Loader for webpack to process CSS with PostCSS
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: function () {
-                                return [
-                                    require('autoprefixer')
-                                ];
-                            }
-                        }
-                    },
-                    {
-                        // Loads a SASS/SCSS file and compiles it to CSS
-                        loader: 'sass-loader'
-                    }
-                ]
-            }
-        ]
     }
 };
