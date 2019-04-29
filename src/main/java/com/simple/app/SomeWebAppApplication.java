bc3cb0bc5dd8be7b16bd1537568ce1c51c98c2ea
japshvincent <japshvincent@gmail.com>
2019-04-29 15:02:11 -0500

    doing some clean up.

--- src/main/java/com/simple/app/SomeWebAppApplication.java
+++ src/main/java/com/simple/app/SomeWebAppApplication.java
@@ -1,9 +1,7 @@
 package com.simple.app;
 
-import com.simple.app.config.MVCConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -11,10 +9,5 @@ public class SomeWebAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SomeWebAppApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(MVCConfig.class);
 	}
 }
